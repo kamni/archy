1ab239729dae36c4c6783e7482d2cfa5e3101fd6
J Leadbetter <j@jleadbetter.com>
2022-12-23 09:53:44 +0100

    [v0.1.0] Parse options from command line args
    In the main script, parse the group, base_dir, and force arguments
    from the command line.

--- archy/main.py
+++ archy/main.py
@@ -1,8 +1,55 @@
-_VERSION_ = '0.0.1'
+"""
+Main entry point into the archy script.
+"""
+import argparse
+import os
+
+__all__ = ['main']
+
+_VERSION_ = '0.1.0'
+_DESCRIPTION_ = """
+Command line tool for archiving files based on group membership.
+The files belonging to group members will be moved from their current location
+to an uncompressed archive in the /tmp directory.
+"""
+_NOTES_ = """
+NOTE: This script must be run as root.
+"""
+
+
+def _parse_args():
+    parser = argparse.ArgumentParser(
+        prog='Archy',
+        description=_DESCRIPTION_,
+        epilog=_NOTES_,
+    )
+    parser.add_argument(
+        'group',
+        type=str,
+        help='GROUP whose files should be archived',
+    )
+    parser.add_argument(
+        '-d',
+        '--base-dir',
+        nargs='?',
+        default=os.getcwd(),
+        type=str,
+        help=(
+            'BASE_DIR to look for files. Cannot be /. '
+            'If not specified, defaults to cwd'
+        )
+    )
+    parser.add_argument(
+        '-f',
+        '--force',
+        action='store_true',
+        help='Force archiving of group, even if another process is running'
+    )
+    return parser.parse_args()
 
 
 def main():
-    print(f'archy-{_VERSION_}')
+    args = _parse_args()
 
 
 if __name__ == '__main__':
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,8 +1,65 @@
 import unittest
+from unittest import mock
 
-class TestParseArgs(unittest.TestCase):
+from archy.main import _parse_args
+
+class ParseArgsTests(unittest.TestCase):
+    """
+    Tests for main._parse_args
+    """
     def test_no_group(self):
-        self.assertFalse(True)
+        sys_args = ['archy']
+        with mock.patch('sys.argv', sys_args):
+            self.assertRaises(SystemExit, _parse_args)
+
+    def test_with_group(self):
+        group_name = 'groupname'
+        sys_args = ['archy', group_name]
+        with mock.patch('sys.argv', sys_args):
+            cmd_args = _parse_args()
+            self.assertEqual(group_name, cmd_args.group)
+
+    def test_with_no_base_dir(self):
+        cwd = '/home'
+        mock_getcwd = mock.MagicMock()
+        mock_getcwd.return_value = cwd
+        sys_args = ['archy', 'groupname']
+        with mock.patch('sys.argv', sys_args):
+            with mock.patch('os.getcwd', mock_getcwd):
+                cmd_args = _parse_args()
+                self.assertEqual(cwd, cmd_args.base_dir)
+
+    def test_with_base_dir_short(self):
+        base_dir = '/home'
+        sys_args = ['archy', 'groupname', '-d', base_dir]
+        with mock.patch('sys.argv', sys_args):
+            cmd_args = _parse_args()
+            self.assertEqual(base_dir, cmd_args.base_dir)
+
+    def test_with_base_dir_long(self):
+        base_dir = '/home'
+        sys_args = ['archy', 'groupname', f'--base-dir={base_dir}']
+        with mock.patch('sys.argv', sys_args):
+            cmd_args = _parse_args()
+            self.assertEqual(base_dir, cmd_args.base_dir)
+
+    def test_with_no_force(self):
+        sys_args = ['archy', 'groupname']
+        with mock.patch('sys.argv', sys_args):
+            cmd_args = _parse_args()
+            self.assertFalse(cmd_args.force)
+
+    def test_with_force_short(self):
+        sys_args = ['archy', 'groupname', '-f']
+        with mock.patch('sys.argv', sys_args):
+            cmd_args = _parse_args()
+            self.assertTrue(cmd_args.force)
+
+    def test_with_force_long(self):
+        sys_args = ['archy', 'groupname', '--force']
+        with mock.patch('sys.argv', sys_args):
+            cmd_args = _parse_args()
+            self.assertTrue(cmd_args.force)
 
 
 if __name__ == '__main__':
